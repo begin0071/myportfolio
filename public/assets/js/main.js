1990de5fcab854ea92f72d23ae66a2b4fba05b89
begin0071 <m.khaki610@gmail.com>
2023-05-25 01:01:02 -0500

    run tailwind

---
+++ public/assets/js/main.js
@@ -0,0 +1,51 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navList = document.querySelector('.nav-list');
+
+// Applies The Class That Transforms The Mobile Menu
+const toggleNav = () => {
+
+    if (navList.classList.contains('nav-list-active')) {
+        navList.classList.toggle('nav-open');
+        setTimeout(() => {
+            navList.classList.toggle('nav-list-active');
+            navList.classList.toggle('nav-list-inactive');
+        }, 350);
+    } else if (navList.classList.contains('nav-list-inactive')) {
+        navList.classList.toggle('nav-list-inactive');
+        navList.classList.toggle('nav-list-active');
+        setTimeout(() => {
+            navList.classList.toggle('nav-open');
+        }, 350);
+    }
+};
+
+
+// Gets The Position Of Section Heading Elements For Side Dot Nav
+const updateList = () => {
+    const titles = [...document.querySelectorAll('h1, h2')].sort((a, b) => {
+        return Math.abs(a.getBoundingClientRect().top) - Math.abs(b.getBoundingClientRect().top);
+    });
+    
+    document.querySelectorAll(".selected-circle").forEach(c => c.classList.remove("selected-circle"));
+	
+	document.querySelectorAll(".nav-dot")[[...document.querySelectorAll('h1, h2')].indexOf(titles[0])].classList.add("selected-circle");
+};
+
+updateList();
+window.addEventListener('scroll', () => {
+    updateList();
+});
+
+// Toggles Mobile Nav When Closed
+navToggle.addEventListener('click', toggleNav);
+
+// Check If Mobile Nav Item Was Pressed
+// Then Closes The Menu If Nav Item Is Pressed
+navList.addEventListener('click', (e) => {
+    e.stopPropagation();
+    if (e.target.classList.contains('nav-item')) {
+        setTimeout(() => {
+            toggleNav();
+        }, 500);
+    };
+});
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html',
+    './public/**/**/*.js',
+    './public/**/*.html'],
+  theme: {
+    extend: {
+      colors: {
+        'body': '#083344',
+        'selected-text': '#fef3c7',
+        'theme': '#0369a1',
+        'nav': '#fef3c7',
+        'secondary': '#fffbeb',
+        'badge': '#3f3f46',
+        'input-border': '#a78bfa',
+        'input': '#1c1917'
+      },
+      fontFamily: {
+        'intervar': ["'Inter var'", 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
